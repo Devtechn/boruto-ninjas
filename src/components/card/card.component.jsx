45522e9f211887fb5d5c2796bf4e29b3438f00b0
Devtechn <71133422+Devtechn@users.noreply.github.com>
2020-10-26 22:02:46 +0100

    Update card.component.jsx

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className="card-container">
-        <img src={'https://raw.githubusercontent.com/Devtechn/boruto-ninjas/master/public/ninjas-pics/1.jpeg'} alt={props.ninja.name} height="160px"/>
+        <img src={`https://raw.githubusercontent.com/Devtechn/boruto-ninjas/master/public/ninjas-pics/${props.ninja.id}.jpeg`} alt={props.ninja.name} height="160px"/>
         <h2>{props.ninja.name}</h2>
     </div>
     
