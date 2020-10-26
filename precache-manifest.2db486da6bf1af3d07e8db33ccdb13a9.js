8316d025e53e766f130b844a4c67848266e10b21
Devtechn <devsocialprofile@gmail.com>
2020-10-26 19:17:50 +0100

    Updates

---
+++ precache-manifest.2db486da6bf1af3d07e8db33ccdb13a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31023515e3533ca4ae6877f434c91d0",
+    "url": "/boruto-ninjas/index.html"
+  },
+  {
+    "revision": "378dad098d23511a097d",
+    "url": "/boruto-ninjas/static/css/main.6d6b01c0.chunk.css"
+  },
+  {
+    "revision": "fff928e45db74e599ac2",
+    "url": "/boruto-ninjas/static/js/2.f04d1202.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/boruto-ninjas/static/js/2.f04d1202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378dad098d23511a097d",
+    "url": "/boruto-ninjas/static/js/main.a78c244c.chunk.js"
+  },
+  {
+    "revision": "b090affd5e2a69c35118",
+    "url": "/boruto-ninjas/static/js/runtime-main.d5939d9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boruto-ninjas/static/media/logo.5d5d9eef.svg"
+  }
+]);
